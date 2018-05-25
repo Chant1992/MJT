589b8a89319945fd093d53d7e74018a1ea250734
Chant <shichangling1992@hotmail.com>
2018-05-25 14:35:47 +0800

    creat project

---
+++ MJTCode/MJTCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MJTCode
+//
+//  Created by Chant on 2018/5/25.
+//  Copyright © 2018年 OLYM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
